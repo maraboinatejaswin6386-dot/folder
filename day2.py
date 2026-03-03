cc950a9b9f14a70d462c8900d05530fad0d47132
maraboinatejaswin6386-dot <maraboinatejaswin6386@gmail.com>
2026-03-03 12:22:22 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,47 @@
+a =10
+b =4
+
+print (a+b) # addition
+print (a-b) #subtraction
+print (a*b) # mutliplication
+print (a**b) # exponentiation
+print (a/b) #division   ----> float division 
+print(a//b) # floor division
+print(a%b) # modulus
+
+#  percentage 
+ 
+target_sales =500
+achieved_sales =450
+percentage = (achieved_sales / target_sales)*100
+print(percentage)
+
+# logical operators
+#and 
+#not 
+#or
+a=5
+b=10
+
+print(a>3 and b<20)
+print(a>3 and b>20)
+
+print(a>3 or b<20)
+print(a>3 or b>20)
+
+print(not(a>3))
+#comparision operators
+
+print (a==b) # equal to 
+print (a!=b) # not equal to 
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+
+
+
+
+
+
